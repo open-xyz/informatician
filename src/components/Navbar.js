44d5814aa157f88e96d966c7262963bf7f0279b4
rohansx <sharmarohan615@gmail.com>
2023-04-22 12:00:24 +0530

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+      return (
+        <div>
+          // Navbar content goes here
+        </div>
+      );
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Book Listing App</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/books">Books</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+
+export default Navbar;
