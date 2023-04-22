0b049bd9c68092c895fa23b381ab2923718e8839
rohansx <sharmarohan615@gmail.com>
2023-04-22 11:49:29 +0530

    Started project

---
+++ src/components/BookList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class BookList extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Book List</h1>
+          <ul>
+            <li>Book 1</li>
+            <li>Book 2</li>
+            <li>Book 3</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  
+export default BookList;
