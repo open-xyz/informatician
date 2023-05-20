e3b7a900db9b21b8e0fc63fb139093ae15cb9a7b
Aditya Tulsiyan <tulsiyanaditya11@gmail.com>
2023-05-20 20:47:57 +0530

    fix:  books components added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import './BookList.css';
+import React, { useState } from "react";
+import "./BookList.css";
 
-class BookList extends React.Component {
-  render() {
-    return (
-      <div className="book-list-container">
-        <h1 className="book-list-title">Book List</h1>
-        <ul className="book-list">
-          <li className="book-list-item">Book 1</li>
-          <li className="book-list-item">Book 2</li>
-          <li className="book-list-item">Book 3</li>
-        </ul>
-      </div>
-    );
-  }
+export default function BookList() {
+  const [books, setBooks] = useState([
+    { name: "Book1" },
+    { name: "Book2" },
+    { name: "Book3" },
+    { name: "Book4" },
+  ]);
+
+  const booksComponent = books.map((book) => (
+    <li className="book-list-item">{book.name}</li>
+  ));
+  return (
+    <div className="book-list-container">
+      <h1 className="book-list-title">Book List</h1>
+      <ul className="book-list">{booksComponent}</ul>
+    </div>
+  );
 }
-
-export default BookList;
