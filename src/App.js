5a7f69656a7b2cc7427806ba86a7692e62b64753
Hemant2335 <knrt73373@gmail.com>
2023-05-20 18:54:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import Navbar from './components/Layout/Navbar/Navbar';
 import BookList from './components/BookList';
-import Footer from './components/Layout/Header/Footer';
-import About from './components/About';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from './components/Layout/Layout';
 class App extends React.Component {
   render() {
     return (
