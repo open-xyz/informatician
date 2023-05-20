c0715dbb8bd74c16ee0ee1c2f318305299e63273
Aditya Tulsiyan <tulsiyanaditya11@gmail.com>
2023-05-20 14:03:12 +0530

    refine: made some changes to refine the pervious code
    Most of the things are kept same. Major focus is put to preparing the project more coder-friendly along with some CSS changes.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import './BookList.css';
+import React, { useState } from "react";
 
-class BookList extends React.Component {
-  render() {
-    return (
+export default function BookList() {
+  const [books, setBooks] = useState([{ name: "Book1" }, { name: "book2" }]);
+
+  let bookComponent = books.map((book) => (
+    <li className="book-list-item">{book.name}</li>
+  ));
+
+  return (
+    <div className="body">
       <div className="book-list-container">
         <h1 className="book-list-title">Book List</h1>
-        <ul className="book-list">
-          <li className="book-list-item">Book 1</li>
-          <li className="book-list-item">Book 2</li>
-          <li className="book-list-item">Book 3</li>
-        </ul>
+        <ul className="book-list">{bookComponent}</ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default BookList;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
-import './Navbar.css';
+import React from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar">
-          <div className="container">
-            <div className="navbar-brand">Book Listing App</div>
-            <div className="navbar-items">
-              <a className="navbar-link" href="/">Home</a>
-              <a className="navbar-link" href="/books">Books</a>
-              <a className="navbar-link" href="/about">About</a>
-            </div>
-          </div>
-        </nav>
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">Informatics Books</div>
+      <div className="navbar-items">
+        <a href="/">Home</a>
+        <a href="/books">Books</a>
+        <a href="/about">About</a>
       </div>
-    );
-  }
+    </nav>
+  );
 }
-
-export default Navbar;
