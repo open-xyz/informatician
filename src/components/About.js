9b99c16c25794d2761007209e49224f4789ff6d1
Deepak Singh <deepkushwah824@gmail.com>
2023-05-20 13:43:12 +0530

    #41 done
    a basic about section designed

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
+import bookImage from "../assets/books.webp";
 
 const About = () => {
   return (
     <div className="about">
       <div className="container">
-        <h2 className="about-title">About Us</h2>
-        <p className="about-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim fringilla nulla, vitae convallis turpis pulvinar sed. Nullam auctor ex at sem aliquam, id laoreet sem porttitor. Morbi in nunc ac nisl interdum posuere vitae id ligula. In consectetur nunc eu feugiat consectetur. Vestibulum sed tortor pretium, congue lectus in, tristique urna. Suspendisse volutpat elit sit amet massa luctus, vel scelerisque ipsum facilisis. Nam pretium posuere quam, nec ultrices urna feugiat eu. Vestibulum vitae elementum ligula.
-        </p>
-        <p className="about-description">
-          Nulla lobortis metus nec eleifend efficitur. Duis dapibus, dui ac interdum hendrerit, tellus tortor hendrerit sapien, eu iaculis mi ex at nisi. Quisque vel tortor magna. Sed eleifend efficitur mi, ac semper lorem vulputate eu. Maecenas condimentum, ipsum ac lacinia cursus, ligula ligula laoreet enim, ut ullamcorper risus risus sit amet lorem. Nulla sagittis scelerisque nisi ut posuere. Sed iaculis fringilla mauris, et tempor lectus ultrices sit amet. Pellentesque sagittis nunc et urna pellentesque fringilla. Nullam volutpat felis a mi cursus, at scelerisque odio commodo. Nam consectetur dictum lacinia. Aenean commodo fringilla dui vel viverra. Sed volutpat tortor vitae erat fringilla, a aliquam ante faucibus. Nullam efficitur metus sed dolor laoreet, ut euismod lectus iaculis. Integer tempus pulvinar risus, vitae pellentesque leo ultrices at.
-        </p>
+        <div className="about-content">
+          <div className="about-text">
+            <h2 className="about-title">About Us</h2>
+            <p className="about-description">
+              At Book Listing App, we believe in the power of books to inspire,
+              educate, and entertain. Books have the ability to transport us to
+              different worlds, expand our horizons, and ignite our imagination.
+              They hold knowledge, stories, and ideas that have the potential to
+              transform lives.
+            </p>
+            <p className="about-description">
+              Our vision is to create a platform that connects book lovers and
+              provides a space for discovering, discussing, and sharing the joy
+              of reading. We aim to foster a vibrant community where readers can
+              explore diverse genres, engage in meaningful conversations, and
+              connect with authors and fellow book enthusiasts.
+            </p>
+            <p className="about-description">
+              As the co-founders of Book Listing App, we are passionate about
+              promoting literacy, encouraging reading habits, and building a
+              community around books. Our mission is to make reading more
+              accessible, enjoyable, and interactive for everyone. We strive to
+              curate a collection of books that cater to various interests and
+              preferences, ensuring there's something for every reader.
+            </p>
+          </div>
+          <div className="about-image">
+            <img src={bookImage} alt="Books" className="book-image" />
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default About;
