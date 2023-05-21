e025388e3782812cbbe79aacbf06afaaa329ec63
ninjahattori21 <115465151+ninjahattori21@users.noreply.github.com>
2023-05-21 13:53:28 +0530

    Resolved all conflict

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import Footer from "./components/common/footer/Footer";
 import BookList from "./pages/books/BookList";
 import About from "./pages/about/About";
 
-export default function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <div className="app-body">
-          <Body />
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <BookList />
+                </>
+              }
+            />
+            <Route path="/about" element={<About />} />
+          </Routes>
+          <Footer />
         </div>
-      </BrowserRouter>
-      <Footer />
-    </div>
-  );
+      </Router>
+    );
+  }
 }
+
+export default App;
