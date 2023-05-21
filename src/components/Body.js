7a483b909ef9c2aea904a1bd7b3b10046a89c489
Deepak Singh <deepkushwah824@gmail.com>
2023-05-22 00:30:07 +0530

    #123
    team page added

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,7 @@ import BookList from "./BookList";
 import About from "./About";
 import Home from "./Home";
 import Contact from "./Contactus";
+import Team from "./Team";
 
 function Body() {
   return (
@@ -13,6 +14,7 @@ function Body() {
         <Route path="/books" element={<BookList />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="/team" element={<Team/>}/>
       </Routes>
     </div>
   );
