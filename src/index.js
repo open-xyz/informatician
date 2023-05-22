d9e2720436431b16446952de4bb149f9560d814e
Deepak Singh <deepkushwah824@gmail.com>
2023-05-22 13:06:01 +0530

    Revert "Navbar Task Completed"

--- src/index.js
+++ src/index.js
@@ -3,21 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Layout from './components/Layout/Layout';
-import { BrowserRouter ,Routes , Route } from 'react-router-dom';
-import About from './components/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route path="/" element={ <App /> }/>
-        <Route path="/about" element={ <About/> }/>
-      </Routes>
-      </Layout>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
