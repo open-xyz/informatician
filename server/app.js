f000753748b09db7f84a92a3b4cbb0d3ce18eed6
Deepak Singh <deepkushwah824@gmail.com>
2023-05-30 02:27:11 +0530

    #231 done

--- server/app.js
+++ server/app.js
@@ -6,21 +6,20 @@ const cron = require("node-cron");
 const { sendNewsletter } = require("./controller/newsletter");
 const newsLetter = require('./router/newsletter');
 
-mongoose.connect(process.env.MONGODB).then(()=>{
-  console.log("DataBase Connected");
-}).catch((err)=>{
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log("Database Connected");
+}).catch((err) => {
   console.log(err);
-})
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api',newsLetter.router)
+app.use('/api', newsLetter.router);
 
 cron.schedule("0 0 * * 0", sendNewsletter);
 
-
-app.listen(process.env.PORT||5000,()=>{
-  console.log("Server Started")
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server Started");
+});
