34732bcda7093718ef0eb2e9ed9e5b269b52aaad
Kartikay Asija <kartikay7057@gmail.com>
2023-05-31 03:53:41 +0530

    search functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, Navigate, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import uploadicon from "../../assets/icon.png";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faArrowUpFromBracket} from '@fortawesome/fontawesome-svg-core/import.macro'
 import NavItems from "./NavItems/NavItems";
+import { searchBooks } from "../../utils/searchBooks";
 
 export default function Navbar() {
   const [clicked, setClicked] = useState(false);
+  const [books,setBooks] = useState([]);
   const location = useLocation();
 
   const handleOptionClick = () => {
     setClicked(false);
+  };
+  const search = async (e) => {
+    try{
+      const result = await searchBooks(e.target.value);
+      result.length>0 && setBooks(result.slice(0,5));
+    }catch(err){
+      throw err;
+    }
   };
 
   return (
@@ -22,7 +32,6 @@ export default function Navbar() {
           className=" h-11 w-15 ml-8  hover:shadow "
           alt=" Logo"
         />
-        
       </Link>
 
       <div className="mobile-nav ">
@@ -49,16 +58,25 @@ export default function Navbar() {
             className="h-8 mr-3 text-bold  hover:font-bold hover:shadow-md self-center  "
           />
         </Link>
-        <form action="" className="search-bar">
-          <input type="text" placeholder="" name="q" />
-          <button type="submit">
-            <i className="fa-solid fa-search"></i>
-          </button>
-        </form>
+        <div>
+          <form className="search-bar">
+            <input type="text" placeholder="" name="q" onChange={search} />
+            <button type="submit">
+              <i className="fa-solid fa-search"></i>
+            </button>
+          </form>
+
+          <div className="w-52 bg-gray-100 absolute mx-auto px-2">
+            {books.length>0 && books.map((book,index)=>(
+              <div className="flex justify-center items-center p-3 border-b-4 my-2 transition duration-200 hover:bg-gray-300" key={index}>
+                <h1>{book?.volumeInfo.title}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
       <div id="mobile">
-      
         <i
           id="bar"
           className={clicked ? "fa-solid fa-times" : "fa-solid fa-bars"}
