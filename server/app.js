879339172bbd867a8b39af7bb10509088a02b131
JainPriya1234 <priya.jain11302@gmail.com>
2023-05-31 01:01:47 +0530

    error handler added

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ const cron = require("node-cron");
 const { sendNewsletter } = require("./controller/newsletter");
 const newsLetter = require('./router/newsletter');
 const mailRouter = require('./router/contact')
+const notFound = require("./errors/customAPIError");
+const errorHandler = require("./errors/customAPIError");
 
 mongoose.connect(process.env.MONGODB).then(() => {
   console.log("Database Connected");
@@ -21,6 +23,11 @@ app.use('/api',newsLetter.router)
 app.use('/sendMail',mailRouter)
 
 cron.schedule("0 0 * * 0", sendNewsletter);
+
+// Error handling
+app.use(notFound);
+app.use(errorHandler);
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server Started");
