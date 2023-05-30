b8117406eb0d716c5391ae2a211b87ce12daf2dd
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2023-05-30 20:14:15 +0530

    rate limiter

--- server/app.js
+++ server/app.js
@@ -1,26 +1,39 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const cron = require("node-cron");
 const { sendNewsletter } = require("./controller/newsletter");
-const newsLetter = require('./router/newsletter');
+const newsLetter = require("./router/newsletter");
+const rateLimiter = require("express-rate-limit");
 
-mongoose.connect(process.env.MONGODB).then(()=>{
-  console.log("DataBase Connected");
-}).catch((err)=>{
-  console.log(err);
-})
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("DataBase Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api',newsLetter.router)
+app.use("/api", newsLetter.router);
+
+const limiter = rateLimiter({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100, //limit each IP for 100 every minute
+  message:
+    "Rate limit exceeded, At the moment we only allow 100 requests per minute",
+});
+
+// Apply the rate limiter middleware to all requests
+app.use(limiter);
 
 cron.schedule("0 0 * * 0", sendNewsletter);
 
-
-app.listen(process.env.PORT||5000,()=>{
-  console.log("Server Started")
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server Started");
+});
