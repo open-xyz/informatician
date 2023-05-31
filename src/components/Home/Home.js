ef1ef197899ae871a2aae55671b3e16b45fddae3
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-05-31 12:19:08 +0530

    Added Category on Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ImgSection from "./ImgSection/ImgSection";
 import BkCards from "./BkCards/BkCards";
+import CgCards from "./CgCards/CgCards";
 
 
 export default function Home() {
   return (
     <>
       <ImgSection />
+      <CgCards />
       <BkCards bkhead="Bestselling Books & Audiobooks" />
       <BkCards 
         bkhead="Current New York Times Bestsellers" />
