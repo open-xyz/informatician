3195a764959ff00e3552da33c128b55dc66001cd
Deepak Singh <deepkushwah824@gmail.com>
2023-06-01 07:07:48 +0530

    #366 done

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,34 +1,32 @@
-import {React, useState} from 'react';
+import React, { useState } from 'react';
 import { FaDiscord, FaTwitter, FaGithub } from 'react-icons/fa';
 import contactImage from '../assets/contact-us.png';
 import './Contactus.css';
 
 const Contact = () => {
-
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [msg, setMsg] = useState('')
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [msg, setMsg] = useState('');
 
   const handleResponse = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     var formData = {
       name: name,
       email: email,
       message: msg
-    }
+    };
 
-    
     await fetch('http://localhost:5000/sendMail', {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(formData)
-    })
+    });
 
-    alert("Contact Request Sent Successfully")
-  }
+    alert('Contact Request Sent Successfully');
+  };
 
   return (
     <div className="contact">
@@ -37,39 +35,35 @@ const Contact = () => {
           <div className="contact-text">
             <h2 className="contact-title">Contact Us</h2>
             <p className="contact-description">
-              We'd love to hear from you! Reach out to us for any inquiries,
-              feedback, or collaboration opportunities.
+              We'd love to hear from you! Reach out to us for any inquiries, feedback, or collaboration opportunities.
             </p>
             <form className="contact-form" onSubmit={handleResponse}>
-              <div
-                style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}
-              >
-                <label style={{ display: 'flex' }}>Full Name</label>
+              <div className="form-field">
+                <label htmlFor="name">Full Name</label>
                 <input
                   type="text"
+                  id="name"
                   placeholder="Name"
                   className="form-input"
                   required
                   onChange={(e) => setName(e.target.value)}
                 />
               </div>
-              <div
-                style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}
-              >
-                <label style={{ display: 'flex' }}>Email</label>
+              <div className="form-field">
+                <label htmlFor="email">Email</label>
                 <input
                   type="email"
+                  id="email"
                   placeholder="Email"
                   className="form-input"
                   required
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div
-                style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}
-              >
-                <label style={{ display: 'flex' }}>Message</label>
+              <div className="form-field">
+                <label htmlFor="message">Message</label>
                 <textarea
+                  id="message"
                   placeholder="Message"
                   className="form-input message-input"
                   required
@@ -82,11 +76,11 @@ const Contact = () => {
             </form>
           </div>
           <div className="contact-image">
-            <img src={contactImage} alt="Contact" className="contact-image" />
+            <img style={{ marginLeft: '10%' }} src={contactImage} alt="Contact" className="contact-image" />
           </div>
         </div>
         <div className="contact-social-icons">
-          <h1 style={{marginTop: 20, marginBottom: 20, fontSize: 30}}>Connect with us:</h1>
+          <h1 style={{ marginTop: 20, marginBottom: 20, fontSize: 30 }}>Connect with us:</h1>
           <div className="social-icons">
             <a href="#!" className="social-icon-twitter">
               <FaTwitter />
@@ -94,7 +88,6 @@ const Contact = () => {
             <a href="#!" className="social-icon-github">
               <FaGithub />
             </a>
-
             <a href="#!" className="social-icon-discord">
               <FaDiscord />
             </a>
