eed2ddb57f7369b2ef06c6b6e92eb423b9f5989d
abhi03ruchi <105661636+abhi03ruchi@users.noreply.github.com>
2023-06-03 18:52:19 +0530

    Enhanced Our Team Page

---
+++ src/components/Team/ContCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Contributor.css';
+const ContCard = (props) => {
+  var x = '';
+  const { image, title, commits, profile} = props;
+  if (commits === 1) {
+    var x = 'commit';
+  } else {
+    var x = 'commits';
+  }
+
+  const openContributorProfile = (url) => {
+    window.open(url, '_blank');
+  };
+
+  return (
+    <>
+      <div
+        className='card' onClick={() => openContributorProfile(profile)}>
+        <img className='profile' src={`https://images.weserv.nl/?output=webp&url=${image}`} alt='Contributor' />
+        <div className='content'>
+          <h1 className='text'>{title}</h1>
+          <p>
+            {commits} {x}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContCard;
---
+++ src/components/Team/Team.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import "./Team.css";
+import teamImage from "../../assets/opensrc.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Contributor from "./Contributor";
+
+
+
+const Team = () => {
+
+
+  useEffect(() => {
+    AOS.init({
+      once: false,
+    });
+    AOS.refresh();
+  }, []);
+  return (
+    <div className="team">
+      {/*         
+        <div className="team-members">
+          <h2 className="team-title">Our Team</h2>
+          <div className="team-member">
+            <img src={teamImage} alt="Team Cartoon" className="team-member-image" />
+          </div>
+        </div>
+
+        <div className="team-description">
+          <p>
+            We are a dedicated team of contributors working on an open-source project. Together, we are passionate about
+            creating innovative solutions and driving positive change in the software development community.
+          </p>
+          <p>
+            Our project welcomes contributions from developers around the world. With the collective effort of the open-source
+            community, we strive to build a robust and feature-rich software that addresses the needs of our users.
+          </p>
+          <p>
+            Join us in this exciting journey and be a part of shaping the future of our project. Together, we can make a
+            difference!
+          </p>
+        </div>
+      </div> */}
+
+
+
+      <div className="w-11/12 flex flex-wrap mx-auto justify-center items-center mt-28 mb-5 max-w-screen-2xl">
+        <div className="" data-aos="fade-left" data-aos-duration="500">
+          <img src={teamImage} className="team-member-image" />
+        </div>
+        <div className="md:w-1/2 ml-5" data-aos="fade-right" data-aos-duration="500">
+          <h1 className="text-5xl mb-7 font-bold">Our Team</h1>
+          <p className="team-description">
+            We are a dedicated team of contributors working on an open-source project. Together, we are passionate about
+            creating innovative solutions and driving positive change in the software development community.
+            Our project welcomes contributions from developers around the world. With the collective effort of the open-source
+            community, we strive to build a robust and feature-rich software that addresses the needs of our users. <br /><br />
+            <a className="submit-btn" href="https://github.com/rohansx/informatician"> Join Us</a>
+          </p>
+        </div>
+
+      </div>
+      <Contributor />
+    </div>
+
+  );
+};
+
+export default Team;
