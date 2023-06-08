5efc153b27ea84ab800eeba19a6e5dfe7b883719
pragatijain <jpragati3003@gmail.com>
2023-06-08 20:38:16 +0530

    Testimonial section updated

--- src/components/About/Testimonial.js
+++ src/components/About/Testimonial.js
@@ -9,8 +9,8 @@ import "aos/dist/aos.css"
 
 export default function Testimonial() {
   return (
-    <div className="mt-20 mb-28">
-      <h1 className="text-center fw-bold fs-1 ">Our Testimonials</h1>
+    <div className="mt-20 mb-28 titles">
+      <h1 className="heading text-center fw-bold fs-1" >Our Testimonials</h1>
       <p className="text-center mt-2 mb-5">
         What Our Client Say About The Books. Reviews And Comments
       </p>
@@ -27,25 +27,27 @@ export default function Testimonial() {
       >
         <SwiperSlide>
           <div className="testimonial max-w-5xl relative w-full py-0 px-0" data-aos="fade-up" data-aos-duration="500">
-            <div className="slide flex items-center justify-center flex-col gap-y-6  py-6 ">
-              <img
-                className="image h-44 w-40 object-cover rounded-full"
+            <div className="slide flex items-center  justify-center flex-col gap-y-6  py-6 ">
+              <img 
+                className="image  h-44 w-40 object-cover rounded-full "
                 src="https://avatars.githubusercontent.com/u/33249782?v=4"
                 alt=""
               />
               <p className="py-0  px-[100px] text-center text-lg font-medium text-slate-800 ">
-                This app is great! Its like the Netflix of books! As a college
+              <i className="bx bxs-quote-alt-left quote-icon text-2xl text-blue-600"></i>
+                   This app is great! Its like the Netflix of books! As a college
                 student, this app has helped me a lot in reading science and
                 mathematics. I have had zero issues and found most of the books
                 I needed. Thanks! I truly love this app!
+                <i className="bx bxs-quote-alt-right quote-icon text-2xl text-blue-600"></i>
               </p>
-              <i className="bx bxs-quote-alt-left quote-icon text-3xl text-blue-600"></i>
+            
               <div className="details flex flex-col items-center">
                 <span className="name text-lg font-semibold text-slate-800">
                   Rohan
                 </span>
                 <span className="job text-base font-normal text-slate-800">
-                  Mentor
+                  Project Admin
                 </span>
               </div>
             </div>
@@ -60,12 +62,14 @@ export default function Testimonial() {
                 alt=""
               />
               <p className="py-0  px-[100px] text-center text-lg font-medium text-slate-800 ">
+              <i className="bx bxs-quote-alt-left quote-icon text-3xl text-blue-600"></i>
                 The selection of ebooks and audiobooks is plentiful. I have
                 found and saved so many books to read and listen to that would
                 have otherwise cost me a fortune anywhere else. There’s no
                 contest at all!
+                <i className="bx bxs-quote-alt-right quote-icon text-2xl text-blue-600"></i>
               </p>
-              <i className="bx bxs-quote-alt-left quote-icon text-3xl text-blue-600"></i>
+              
               <div className="details flex flex-col items-center">
                 <span className="name text-lg font-semibold text-slate-800">
                   Deepak
@@ -86,11 +90,13 @@ export default function Testimonial() {
                 alt=""
               />
               <p className="py-0  px-[100px] text-center text-lg font-medium text-slate-800 ">
+              <i className="bx bxs-quote-alt-left quote-icon text-3xl text-blue-600"></i>
                 From major literary works and detailed historical and scientific
                 studies to popular fiction and self-help books, it’s incredible
                 how much there is to find on Informatician. Fantastic!
+                <i className="bx bxs-quote-alt-right quote-icon text-3xl text-blue-600"></i>
               </p>
-              <i className="bx bxs-quote-alt-left quote-icon text-3xl text-blue-600"></i>
+             
               <div className="details flex flex-col items-center">
                 <span className="name text-lg font-semibold text-slate-800">
                   Sumit
