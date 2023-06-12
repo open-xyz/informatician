a24bdc989af49184d39c9a81de01428f05ce8372
Deepak Singh <deepkushwah824@gmail.com>
2023-06-12 16:40:08 +0530

    #523 added

---
+++ src/components/Categories/biography.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './biography.css'; // Import the CSS file for styling
+
+function biography() {
+  return (
+    <div className="container">
+     <br></br>
+    <br></br>
+    <br></br>
+      <h1>Deepak is working on this page</h1>
+      <p>Please check back later.</p>
+    </div>
+  );
+}
+
+export default biography;
