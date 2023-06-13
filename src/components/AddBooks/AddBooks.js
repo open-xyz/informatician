db737eb80ee3c591a3871d6527a628c3f73e70d9
dhingra-madhav <21BCS10568@cuchd.in>
2023-06-13 14:45:28 +0530

    added state management

--- src/components/AddBooks/AddBooks.js
+++ src/components/AddBooks/AddBooks.js
@@ -1,60 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./AddBooks.css";
-import BookImg from "../../assets/book2.jpg";
 import Upload from "./Upload";
+import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 
 const AddBooks = () => {
+  const navigate = useNavigate();
+
+  const [bookName, setBookName] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publisher, setPublisher] = useState('');
+  const [pages, setPages] = useState('');
+  
+  const handlePages = (e) => {
+    const numOfPages = e.target.value;
+    if (numOfPages === '' || parseInt(numOfPages) >= 1)
+      setPages(numOfPages);
+  };
+
+  const handleAddBooks = (e) => {
+    e.preventDefault();
+
+    if (!bookName || !author || !publisher || !pages) {
+      alert("Please fill in all the fields.");
+    } else {
+      navigate('/success');
+    }
+  };
+
   return (
     <div>
+      <div>
+        <div className="quote text-center">Unleash the Power of Sharing:<br />Upload and Enrich Our Open Source Book Community!</div>
+      </div>
       <div className="addBooks-container">
         <div className="add-books text-white">
-          <h1 className="text-dark form-title">Add Books</h1>
-          <div className="group">
-            <input
-              id="book_name"
-              type="text"
-              className="input"
-              placeholder="Book Name"
-            />
-          </div>
-          <div className="group">
-            <input
-              id="author_name"
-              type="text"
-              className="input"
-              placeholder="Author Name"
-            />
-          </div>
-          <div className="group">
-            <input
-              id="publisher"
-              type="text"
-              className="input"
-              placeholder="Publisher"
-            />
-          </div>
-          <div className="group">
-            <input
-              id="pages"
-              type="number"
-              className="input"
-              placeholder="Pages"
-            />
-          </div>
-          <div className="group">
-            <input
-              id="img_url"
-              type="text"
-              className="input"
-              placeholder="Img_url"
-            />
-          </div>
-          <div className="group-btn">
-            <input type="submit" className="button" value="Add Books" />
-          </div>
+          <h1 className="text-dark form-title">Share Book Information Here! <AutoStoriesOutlinedIcon className="ml-2" /></h1>
+          <form>
+            <div className="group">
+              <label htmlFor="bookName">Book Name:</label>
+              <input
+                id="bookName"
+                type="text"
+                className="input"
+                placeholder="Please enter the book name"
+                value={bookName}
+                onChange={(e) => setBookName(e.target.value)}
+              />
+            </div>
+            <div className="group">
+              <label htmlFor="author">Author Name:</label>
+              <input
+                id="author"
+                type="text"
+                className="input"
+                placeholder="Please enter the author name"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+            </div>
+            <div className="group">
+              <label htmlFor="publisher">Publisher Name:</label>
+              <input
+                id="publisher"
+                type="text"
+                className="input"
+                placeholder="Please enter the publisher name"
+                value={publisher}
+                onChange={(e) => setPublisher(e.target.value)}
+              />
+            </div>
+            <div className="group">
+              <label htmlFor="pages">No. of Pages:</label>
+              <input
+                id="pages"
+                type="number"
+                className="input"
+                placeholder="Please enter the no. of pages"
+                min="1"
+                value={pages}
+                onChange={handlePages}
+              />
+            </div>
+            <div className="group">
+              <label htmlFor="imgURL">Image URL:</label>
+              <input
+                id="imgURL"
+                type="text"
+                className="input"
+                placeholder="Please enter the URL of cover page image"
+              />
+            </div>
+            <Upload />
+            <div className="group-btn">
+              <button className="button" type="submit" onClick={handleAddBooks}>
+                Add Book
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-      <Upload />
     </div>
   );
 };
