d3a39701916923225c04bdf392c7b39e4c2e0a39
dhingra-madhav <21BCS10568@cuchd.in>
2023-06-13 14:47:17 +0530

    added drag and drop file feature

--- src/components/AddBooks/Upload.js
+++ src/components/AddBooks/Upload.js
@@ -1,65 +1,67 @@
-import React from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import "./Upload.css";
 import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 import CloseIcon from "@mui/icons-material/Close";
 
 export default function Upload() {
   const [books, setBooks] = useState([]);
+  const [isDragActive, setIsDragActive] = useState(false);
 
-  function addBook(e) {
-    const newBook = e.target.files[0].name.split(".")[0];
-
-    setBooks((prevBooks) => {
-      return [...prevBooks, newBook];
-    });
+  function addBook(newBook) {
+    setBooks((prevBooks) => [...prevBooks, newBook]);
   }
 
   function deleteBook(id) {
-    setBooks((prevBooks) => {
-      return prevBooks.filter((book, ind) => {
-        return ind !== id;
-      });
-    });
+    setBooks((prevBooks) => prevBooks.filter((book, ind) => ind !== id));
+  }
+
+  function handleDragEnter(e) {
+    e.preventDefault();
+    setIsDragActive(true);
+  }
+
+  function handleDragLeave(e) {
+    e.preventDefault();
+    setIsDragActive(false);
+  }
+
+  function handleDrop(e) {
+    e.preventDefault();
+    setIsDragActive(false);
+
+    const file = e.dataTransfer.files[0];
+    const newBook = file.name.split(".")[0];
+    addBook(newBook);
   }
 
   return (
-    <div
-      className="upload p-10 d-flex flex-column justify-content-center
-    align-items-center"
-    >
-      <h1 className="fs-1 fw-bolder">Upload Books</h1>
+    <div className="upload p-10 d-flex flex-column justify-content-center align-items-center">
+      <h1>Upload Book Here!</h1>
       <div
-        className="upload-box my-5 p-10 d-flex flex-column justify-content-center
-    align-items-center"
+        className={`upload-box my-5 p-10 d-flex flex-column justify-content-center align-items-center ${
+          isDragActive ? "drag-active" : ""
+        }`}
+        onDragEnter={handleDragEnter}
+        onDragOver={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
       >
-        <input
-          type="file"
-          name=""
-          id="upload-btn"
-          accept=".pdf, .txt, .doc, .ppt, .xls, .docx"
-          onChange={addBook}
-        />
-        <label className="mb-5" id="label" for="upload-btn">
+        <label className="mb-4" htmlFor="upload-btn">
           Select Books To Upload <LibraryBooksIcon className="ml-2" />
         </label>
-        {books.map((book, ind) => {
-          return (
-            <p className="uploaded m-1" key={ind}>
-              {book}{" "}
-              <CloseIcon
-                className="close-icon ml-10 cursor-pointer"
-                onClick={(handleClick) => {
-                  deleteBook(ind);
-                }}
-              />
-            </p>
-          );
-        })}
-        <Link to="/success">
-          <button className="mt-4">Submit</button>
-        </Link>
+        <p className="drag-drop text-center">
+          OR<br />
+          Drag and drop files here
+        </p>
+        {books.map((book, ind) => (
+          <p className="uploaded mt-4" key={ind}>
+            {book}{" "}
+            <CloseIcon
+              className="close-icon ml-10 cursor-pointer"
+              onClick={() => deleteBook(ind)}
+            />
+          </p>
+        ))}
       </div>
       <p>Supported file types: pdf, txt, doc, ppt, xls, docx, and more</p>
     </div>
