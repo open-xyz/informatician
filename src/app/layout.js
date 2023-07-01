c6669a6bfc2fbd95479234e626b94cb44cf90b3d
Tushar Saini <s2sharpit@gmail.com>
2023-07-02 04:44:55 +0530

    implementing footer and newsletter

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   )
