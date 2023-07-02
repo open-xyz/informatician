2323231d3f8cbc1f4490d46ed812e7db2ce63278
Tushar Saini <s2sharpit@gmail.com>
2023-07-03 01:30:35 +0530

    changing logo when the theme is change
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- components/Logo.js
+++ components/Logo.js
@@ -1,22 +1,24 @@
-'use client'
-
-import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function Logo() {
-      const {theme} = useTheme();
-
-      const themeImage = `/assets/logos/${
-    theme === "dark" ? "dark" : "light"
-  }_logo.png`;
     return (
-      <Image
-        loading="lazy"
-        src={themeImage}
-        width={300}
-        height={100}
-        className=" h-12 w-fit ml-1 mb-3  hover:shadow "
-        alt="Informatician logo"
-      />
+      <>
+        <Image
+          loading="lazy"
+          src={"/assets/logos/dark_logo.png"}
+          width={300}
+          height={100}
+          className="hidden dark:block h-12 w-fit ml-1 mb-3  hover:shadow "
+          alt="Informatician logo"
+        />
+        <Image
+          loading="lazy"
+          src={"/assets/logos/light_logo.png"}
+          width={300}
+          height={100}
+          className="dark:hidden h-12 w-fit ml-1 mb-3  hover:shadow "
+          alt="Informatician logo"
+        />
+      </>
     );
 }
