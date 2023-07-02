2b47f405ec191f44c8435aa02ee55e2a0bbebcac
Tushar Saini <s2sharpit@gmail.com>
2023-07-02 15:17:41 +0530

    starting files for nextjs migrate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {
-      colors: {
-        customColor: '#87064b',
-      },
-      flex: {
-        '2': '0 0 50%'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
-
