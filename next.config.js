cf7f0c8be013adac8ada992b4049a80da82135e1
Your Name <you@example.com>
2023-07-03 18:46:19 +0600

    added hero section component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental:{
+        appDir:true,
+        serverComponentsExternalPackages:['mongoose'],
+    },
+    webpack(config){
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait:true
+        }
+        return config
+    }
+}
 
 module.exports = nextConfig
