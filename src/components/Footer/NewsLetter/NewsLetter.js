f49d326f408b154a3dcfc77d1b79e59fb29047b5
Pragati Jain <132778037+Pragati-3003@users.noreply.github.com>
2023-07-04 18:52:40 +0530

    Update NewsLetter.js

--- src/components/Footer/NewsLetter/NewsLetter.js
+++ src/components/Footer/NewsLetter/NewsLetter.js
@@ -35,18 +35,24 @@ class NewsLetter extends React.Component {
     };
     render() {
         return (
-            <>
-                <div className="col-md-8 subscribe w-full">
-                    <h4 className="head">Subscribe To NewsLetter</h4>
-                        <div className="d-flex p-2" >
-                            <input type="email" className="form-control" id="simple-search" placeholder="email@example.com" required  />
-                                <button onClick={this.handleClick} type="button" className="btn btn-danger" style={{ marginLeft: '8px',color: 'black' }}>
-                                Subscribe
-                            </button>
+                    <div className="news">
+                        <div className="black">
+                            <div className="container">
+                                <div className="row justify-content-center">
+                                    <div className="col-md-8 subscribe">
+                                        <h4 className="head">Subscribe To NewsLetter</h4>
+                                        <div className="d-flex p-2">
+                                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email ID" />
+                                            <button type="button" className="btn btn-danger" style={{marginLeft: '8px'}}>
+                                                Subscribe
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                </div>
-            </>
-        );
+                    </div>
+                );
     }
 }
 
