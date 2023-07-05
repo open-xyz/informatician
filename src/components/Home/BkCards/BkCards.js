b2542a6e611421928a3a790933bfd7181a24faa2
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-07-05 11:45:51 +0530

    bkCards Code Refactor

--- src/components/Home/BkCards/BkCards.js
+++ src/components/Home/BkCards/BkCards.js
@@ -5,19 +5,9 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "./BkCards.css";
 import CardContent from "./CardContent/CardContent";
-import ebk1 from "../../../assets/ebooks/ebk1.webp";
-import ebk2 from "../../../assets/ebooks/ebk2.webp";
-import ebk3 from "../../../assets/ebooks/ebk3.webp";
-import ebk4 from "../../../assets/ebooks/ebk4.webp";
-import ebk5 from "../../../assets/ebooks/ebk5.webp";
-import ebk6 from "../../../assets/ebooks/ebk6.webp";
-import abk1 from "../../../assets/audiobks/abk1.webp";
-import abk2 from "../../../assets/audiobks/abk2.webp";
-import abk3 from "../../../assets/audiobks/abk3.webp";
-import abk4 from "../../../assets/audiobks/abk4.webp";
-import SingleBk from "../SingleBk/SingleBk";
 import { useSwiper } from "swiper/react";
 import { Link } from "react-router-dom";
+import Books from "./Books";
 
 SwiperCore.use([Autoplay]);
 
@@ -47,10 +37,6 @@ const GhostNextButton = ({ refprop }) => {
 export default function BkCards(props) {
   const prevButton = useRef(null);
   const nextButton = useRef(null);
-
-  const singleBookView = () => {
-    return <SingleBk />;
-  };
 
   const swiperRef = useRef(null);
 
@@ -151,116 +137,22 @@ export default function BkCards(props) {
                   background: props.theme === "dark" ? '#161313' : ''}}
               >
                 <GhostPrevButton refprop={prevButton} />
-                <SwiperSlide > 
-                  <Link to="books/book">
+                {
+                  Books.map((book)=>{
+                    return <SwiperSlide > 
+                  <Link to={book.link}>
                   <CardContent  className="columnn"
-                    imgName={ebk1}
-                    type="Ebook"
-                    title="The Perfect Marriage: A Completely Gripping Psychological Suspense"
-                    author="Jeneva Rose"
-                    description="A gripping psychological suspense unveiling the dark secrets of a perfect marriage."
+                    imgName={book.img}
+                    type={book.type}
+                    title={book.title}
+                    author={book.author}
+                    description={book.description}
                   />
                   </Link>
                 </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book1">
-                  <CardContent
-                    imgName={abk1}
-                    type="Audiobook"
-                    title="The Last Mrs. Parrish: A Novel"
-                    author="Liv Constantine"
-                    description="A gripping tale of manipulation and revenge in The Last Mrs. Parrish: A Novel."
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book2">
-                  <CardContent
-                    imgName={abk2}
-                    type="Audiobook"
-                    title="The Guest List: A Novel"
-                    author="Lucy Foley"
-                    description="A deadly wedding celebration unfolds on a remote island in The Guest List: A Novel."
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book3">
-                  <CardContent
-                    imgName={abk3}
-                    type="Audiobook"
-                    title="The Subtle Art of Not Giving a F*ck: A Counterintuitive Approach to Living a Good Life"
-                    author="Mark Manson"
-                    description="Embracing the liberating philosophy of prioritizing what truly matters in life."
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book4">
-                  <CardContent
-                    imgName={abk4}
-                    type="Audiobook"
-                    title="Building a StoryBrand: Clarify Your Message So Customers Will Listen"
-                    author="Findaway"
-                    description="Transform your marketing with the power of storytelling in Building a StoryBrand."
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book5">
-                  <CardContent
-                    imgName={ebk2}
-                    type="Ebook"
-                    title="An Ugly Truth: Inside Facebook's Battle for Domination"
-                    author="Sheera Frenkel"
-                    description="Unveiling Facebook's unsettling rise, An Ugly Truth takes readers inside the battle for domination."
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book6">
-                  <CardContent
-                    imgName={ebk3}
-                    type="Ebook"
-                    title="Never Split the Difference: Negotiating As If Your Life Depended On It"
-                    author="Chris Voss"
-                    description="Master the art of high-stakes negotiation in Never Split the Difference"
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book7">
-                  <CardContent
-                    imgName={ebk4}
-                    type="Ebook"
-                    title="Everything Is F*cked: A Book About Hope"
-                    author="Mark Manson"
-                    description="Discover hope in a chaotic world with Everything Is F*cked: A Book About Hope."
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book8">
-                  <CardContent
-                    imgName={ebk5}
-                    type="Ebook"
-                    title="The Intelligent Investor, Rev. Ed: The Definitive Book on Value Investing"
-                    author="Benjamin Graham"
-                    description="The ultimate guide to value investing, offering timeless wisdom for financial success."
-                  />
-                  </Link>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Link to="books/book9">
-                  <CardContent
-                    imgName={ebk6}
-                    type="Ebook"
-                    title="Vanderbilt: The Rise and Fall of an American Dynasty"
-                    author="Anderson Cooper"
-                    description="Chronicles the captivating story of the Vanderbilt family, from their astonishing ascent to their eventual downfall."
-                  />
-                  </Link>
-                </SwiperSlide>
+                  })
+                }
+                
               <GhostNextButton refprop={nextButton} />
               </Swiper>
               
