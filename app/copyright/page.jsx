c7cd3430c18b3d6f153b7045e58832e4169bfdc3
Your Name <you@example.com>
2023-07-08 03:40:56 +0600

    Added Copyright Page

---
+++ app/copyright/page.jsx
@@ -0,0 +1,77 @@
+import "./Copyright.css";
+
+const CopyrightPage = () => {
+  return (
+    <div className="copyright">
+      <div className="copyright-container">
+        <h2 className="copyright-title">Copyright Notice</h2>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">Ownership of Content</h3>
+          <div className="copyright-content">
+            <p>
+            All content on this website, including text, graphics, logos, images, audio clips, and software, is the property of <strong><i> Informatician </i></strong>or its content suppliers and is protected by international copyright laws.
+            </p>
+          </div>
+        </div>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">Ownership of Compilation</h3>
+          <div className="copyright-content">
+            <p>
+            The compilation (meaning the collection, arrangement, and assembly) of all content on this website is the exclusive property of <strong><i> Informatician </i></strong> and is protected by international copyright laws.
+            </p>
+          </div>
+        </div>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">Restrictions on Use</h3>
+          <div className="copyright-content">
+            <p>
+            Except as expressly permitted on this website, you may not copy, reproduce, distribute, republish, download, display, post, or transmit any content, in any form or by any means, including but not limited to electronic, mechanical, photocopying, recording, or otherwise, without the prior written permission of <strong><i> Informatician </i></strong> or the respective copyright owner.
+            </p>
+          </div>
+        </div>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">External Links</h3>
+          <div className="copyright-content">
+            <p>
+              Book Listing App may contain links to external websites or third-party services that are not operated by us. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party websites or services.
+            </p>
+          </div>
+        </div>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">Trademarks</h3>
+          <div className="copyright-content">
+            <p>
+            All trademarks, service marks, trade names, and logos used on this website are the property of their respective owners and may not be used without permission.
+            </p>
+          </div>
+        </div>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">Disclaimer</h3>
+          <div className="copyright-content">
+            <p>
+            The information provided on this website is for general informational purposes only. We make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability of the information contained on the website.
+            </p>
+          </div>
+        </div>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">Changes to the Copyright Page</h3>
+          <div className="copyright-content">
+            <p>
+            We reserve the right to modify or update this copyright page at any time without prior notice. Changes will be effective immediately upon posting the updated version on this website.
+            </p>
+          </div>
+        </div>
+        <div className="copyright-section">
+          <h3 className="copyright-section-title">Contact Us</h3>
+          <div className="copyright-content">
+            <p>
+              If you have any questions or concerns about this Privacy Policy, please contact us at <a href="mailto:informaticianx@gmail.com" className="contact-email">informaticianx@gmail.com</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CopyrightPage;
