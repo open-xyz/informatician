50b11a0fb88c69089271fad9f42ee0a40ef9e783
Aditya Tulsiyan <72987111+adity2911@users.noreply.github.com>
2023-07-09 11:31:09 +0530

    Fix: Navbar Design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,51 @@ import darkLogo from "../../assets/logos/dark_logo.png";
 const Footer = () => {
   let year = new Date().getFullYear();
 
+  const footerItems = [
+    [
+      { itemName: "About Us", itemLinks: "/about" },
+      { itemName: "Our Team", itemLinks: "/team" },
+      { itemName: "Contact Us", itemLinks: "/contact" },
+      { itemName: "Join our team!", itemLinks: "/join" },
+    ],
+    [
+      { itemName: "Help/FAQ", itemLinks: "/faq" },
+      { itemName: "Accessibility", itemLinks: "/accessibility" },
+      { itemName: "Publishers", itemLinks: "/publishers" },
+      { itemName: "AdChoices", itemLinks: "/adchoices" },
+    ],
+    [
+      { itemName: "Privacy Policy", itemLinks: "/privacy" },
+      { itemName: "Terms & Conditions", itemLinks: "/terms" },
+      { itemName: "CopyRight", itemLinks: "/copyright" },
+      { itemName: "Cookie Preference", itemLinks: "/cookie" },
+    ],
+  ];
+
+  const columnNames = ["About", "Support", "Legal"];
+
+  const footerComponents = footerItems.map((coulmnItems, index) => {
+    return (
+      <div key={index}>
+        <h2 className={`mb-6 text-m font-bold`}>{columnNames[index]}</h2>
+        <ul className="font-medium">
+          {coulmnItems.map((item, index) => {
+            return (
+              <li key={index} className="mb-4">
+                <Link to={item.itemLinks}>{item.itemName}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  });
+
   return (
     <>
       <NewsLetter />
       <footer>
-        <div className=" footer_outer mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="footer_body">
             <div className=" footer_main mb-6 md:mb-0">
               <Link to="/" className="logo_img">
@@ -23,63 +63,15 @@ const Footer = () => {
               </Link>
             </div>
             <div className="footer_inner_body">
-              <div>
-                <h2 className={`mb-6 text-m font-bold`}>About</h2>
-                <ul className="font-medium">
-                  <li className="mb-4">
-                    <Link to="./about">About us</Link>
-                  </li>
-                  <li className="mb-4">
-                    <Link to="./team">Our Team</Link>
-                  </li>
-                  <li className="mb-4">
-                    <Link to="./contact">Contact Us</Link>
-                  </li>
-                  <li>
-                    <Link to="/join">Join our team!</Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className={`mb-6 text-m font-bold `}>Support</h2>
-                <ul className="  font-medium">
-                  <li className="mb-4">
-                    <Link to="./faq">Help/FAQ</Link>
-                  </li>
-                  <li className="mb-4">
-                    <Link to="/accessibility">Accessibility</Link>
-                  </li>
-                  <li className="mb-4">
-                    <Link to="/publishers">Publishers</Link>
-                  </li>
-                  <li>
-                    <Link to="/adchoices">AdChoices</Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className={`mb-6 text-m font-bold `}>Legal</h2>
-                <ul className="  font-medium">
-                  <li className="mb-4">
-                    <Link to="./privacy">Privacy Policy</Link>
-                  </li>
-                  <li className="mb-4">
-                    <Link to="./terms">Terms &amp; Conditions</Link>
-                  </li>
-                  <li className="mb-4">
-                    <Link to="./copyright">Copyright</Link>
-                  </li>
-                  <li>
-                    <Link to="./cookie">Cookie Preference</Link>
-                  </li>
-                </ul>
-              </div>
+              {footerComponents.map((item, index) => {
+                return (
+                  <div key={index} className="footer_inner_body_item">
+                    {item}
+                  </div>
+                );
+              })}
             </div>
           </div>
-          <hr
-            className="my-4 sm:mx-auto  lg:my-8"
-            style={{ borderColor: "var(--black)" }}
-          />
           <div className="copyright_footer">
             <span className={`text-sm font-bold   sm:text-center `}>
               © {year}{" "}
@@ -89,25 +81,14 @@ const Footer = () => {
               . All Rights Reserved.
             </span>
             <div className="flex mt-0 space-x-6 sm:justify-center sm:mt-0 mr-14 2xl:mr-0">
-              <Link
-                to="/develop"
-                target="_blank"
-                className={` hover:text-[#00acee]`}
-              >
+              <Link to="/develop">
                 <BsTwitter className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
-                <span className="sr-only">Twitter page</span>
               </Link>
-              <Link
-                to="https://github.com/rohansx/informatician"
-                target="_blank"
-                className={` hover:text-[#12BC00]`}
-              >
+              <Link to="https://github.com/rohansx/informatician">
                 <BsGithub className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
-                <span className="sr-only">GitHub account</span>
               </Link>
-              <Link to="#" className={` hover:fill-[#5865F2]`}>
+              <Link to="#">
                 <BsDiscord className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
-                <span className="sr-only">Dribbble account</span>
               </Link>
             </div>
           </div>
