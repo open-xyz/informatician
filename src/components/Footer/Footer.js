535ce92794ebbef897cd4b888d4d5d1145353409
Aditya Tulsiyan <72987111+adity2911@users.noreply.github.com>
2023-07-09 10:37:51 +0530

    Fix: Navbar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,163 +1,85 @@
-import React, { useEffect } from "react";
-import NewsLetter from "./NewsLetter/NewsLetter";
-// import  "./NewsLetter/NewsLetter.css"
-import { Link, useLocation } from "react-router-dom";
-import { BsDiscord, BsGithub, BsTwitter } from "react-icons/bs";
 import "./Footer.css";
-import lightLogo from "../../assets/logos/light_logo.png";
+import NewsLetter from "./NewsLetter/NewsLetter";
+import { Link } from "react-router-dom";
+import { BsDiscord, BsGithub, BsTwitter } from "react-icons/bs";
 import darkLogo from "../../assets/logos/dark_logo.png";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-}
-const Footer = (props) => {
+const Footer = () => {
   let year = new Date().getFullYear();
-
-  const themeImage = props.theme === "dark" ? darkLogo : lightLogo;
 
   return (
     <>
-      <ScrollToTop />
       <NewsLetter />
       <footer>
         <div className=" footer_outer mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-    <div className="footer_body">
-    <div  className=" footer_main mb-6 md:mb-0">
-              <Link to="/"  className="logo_img">
+          <div className="footer_body">
+            <div className=" footer_main mb-6 md:mb-0">
+              <Link to="/" className="logo_img">
                 <img
-                src= {darkLogo}
-                className=" h-12 w-17 ml-1 mb-2.5  hover:shadow " 
-                alt="Informatician logo"
-              />
+                  src={darkLogo}
+                  className=" h-12 w-17 ml-1 mb-2.5  hover:shadow "
+                  alt="Informatician logo"
+                />
               </Link>
             </div>
             <div className="footer_inner_body">
               <div>
-                <h2 className={`mb-6 text-m font-bold`}>
-                  About
-                </h2>
+                <h2 className={`mb-6 text-m font-bold`}>About</h2>
                 <ul className="font-medium">
                   <li className="mb-4">
-                    <Link
-                      to="./about"
-                      className="transition-all duration-300 ease-in-out"
-                    >
-                      About us
-                    </Link>
+                    <Link to="./about">About us</Link>
                   </li>
                   <li className="mb-4">
-                    <Link
-                      to="./team"
-                      className="transition-all duration-300 ease-in-out"
-                    >
-                      Our Team
-                    </Link>
+                    <Link to="./team">Our Team</Link>
                   </li>
                   <li className="mb-4">
-                    <Link
-                      to="./contact"
-                      className="transition-all duration-300 ease-in-out "
-                    >
-                      Contact Us
-                    </Link>
+                    <Link to="./contact">Contact Us</Link>
                   </li>
                   <li>
-                    <Link
-                      to="/join"
-                      className="transition-all duration-300 ease-in-out "
-                    >
-                      Join our team!
-                    </Link>
+                    <Link to="/join">Join our team!</Link>
                   </li>
                 </ul>
               </div>
               <div>
-                <h2 className={`mb-6 text-m font-bold `}>
-                  Support
-                </h2>
+                <h2 className={`mb-6 text-m font-bold `}>Support</h2>
                 <ul className="  font-medium">
                   <li className="mb-4">
-                    <Link
-                      to="./faq"
-                      className="transition-all duration-300 ease-in-out "
-                    >
-                      Help/FAQ
-                    </Link>
+                    <Link to="./faq">Help/FAQ</Link>
                   </li>
                   <li className="mb-4">
-                    <Link
-                      to="/accessibility"
-                      className="transition-all duration-300 ease-in-out "
-                    >
-                      Accessibility
-                    </Link>
+                    <Link to="/accessibility">Accessibility</Link>
                   </li>
                   <li className="mb-4">
-                    <Link
-                      to="/publishers"
-                      className="transition-all duration-300 ease-in-out  "
-                    >
-                      Publishers
-                    </Link>
+                    <Link to="/publishers">Publishers</Link>
                   </li>
                   <li>
-                    <Link
-                      to="/adchoices"
-                      className="transition-all duration-300 ease-in-out "
-                    >
-                      AdChoices
-                    </Link>
+                    <Link to="/adchoices">AdChoices</Link>
                   </li>
                 </ul>
               </div>
               <div>
-                <h2 className={`mb-6 text-m font-bold `}>
-                  Legal
-                </h2>
+                <h2 className={`mb-6 text-m font-bold `}>Legal</h2>
                 <ul className="  font-medium">
                   <li className="mb-4">
-                    <Link
-                      to="./privacy"
-                      className="transition-all duration-300 ease-in-out"
-                    >
-                      Privacy Policy
-                    </Link>
+                    <Link to="./privacy">Privacy Policy</Link>
                   </li>
                   <li className="mb-4">
-                    <Link
-                      to="./terms"
-                      className="transition-all duration-300 ease-in-out"
-                    >
-                      Terms &amp; Conditions
-                    </Link>
+                    <Link to="./terms">Terms &amp; Conditions</Link>
                   </li>
                   <li className="mb-4">
-                    <Link
-                      to="./copyright"
-                      className="transition-all duration-300 ease-in-out"
-                    >
-                      Copyright
-                    </Link>
+                    <Link to="./copyright">Copyright</Link>
                   </li>
                   <li>
-                    <Link
-                      to="./cookie"
-                      className="transition-all duration-300 ease-in-out"
-                    >
-                      Cookie Preference
-                    </Link>
+                    <Link to="./cookie">Cookie Preference</Link>
                   </li>
                 </ul>
               </div>
             </div>
-       
-    </div>
-    <hr className="my-4 sm:mx-auto  lg:my-8" style={{ borderColor: "var(--black)" }} />
+          </div>
+          <hr
+            className="my-4 sm:mx-auto  lg:my-8"
+            style={{ borderColor: "var(--black)" }}
+          />
           <div className="copyright_footer">
             <span className={`text-sm font-bold   sm:text-center `}>
               © {year}{" "}
@@ -167,7 +89,11 @@ const Footer = (props) => {
               . All Rights Reserved.
             </span>
             <div className="flex mt-0 space-x-6 sm:justify-center sm:mt-0 mr-14 2xl:mr-0">
-              <Link to="/develop" target="_blank" className={` hover:text-[#00acee]`} >
+              <Link
+                to="/develop"
+                target="_blank"
+                className={` hover:text-[#00acee]`}
+              >
                 <BsTwitter className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
                 <span className="sr-only">Twitter page</span>
               </Link>
@@ -176,7 +102,7 @@ const Footer = (props) => {
                 target="_blank"
                 className={` hover:text-[#12BC00]`}
               >
-                < BsGithub className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
+                <BsGithub className="w-6 h-6 transition-all duration-200 ease-in-out transform hover:scale-110" />
                 <span className="sr-only">GitHub account</span>
               </Link>
               <Link to="#" className={` hover:fill-[#5865F2]`}>
