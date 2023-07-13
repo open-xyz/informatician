46e5ec1bcef750bf48138a6dda903afd7a2bfdcc
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-07-13 19:39:45 +0530

    Validation in add book form

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -16,7 +16,7 @@ const validate = {
           : { email: true, emailError: "Please enter a valid email address" }
       },
     message: (value)=>{
-        const numOfWords = value.split(" ").length;
+        const numOfWords = value.split(/\s+/).length;
     
         if(numOfWords < 10 || numOfWords > 100){
           return {message: true, messageError: "Describe your message in 10 to 100 words"}
@@ -24,6 +24,35 @@ const validate = {
           return {message: false, messageError: false}
         }
       },
+
+      bookName: (value)=>{
+        const numOfWords = value.split(/\s+/).length;
+    
+        if(numOfWords > 10){
+          return {bookName: true, bookNameError: "Book title can contain maximum 10 words"}
+        }
+        else{
+          return {bookName: false, bookNameError: false}
+        }
+      },
+
+      authorName: (value)=>{
+        const numOfWords = value.split(/\s+/).length;
+        if(numOfWords < 2)  return {authorName: true, authorNameError: "Add First Name and Last Name"}
+        if(value.length > 40) return {authorName: true, authorNameError: "Name must be less than 40 characters"}
+        return {authorName: false, authorNameError: false}
+      },
+
+      publisher: (value)=>{
+        if(value.length > 30) return {publisher: true, publisherError: "Publisher name must be less than 15 characterss"}
+        return {publisher: false, publisherError: false}
+      },
+
+      pages: (value)=>{
+          if(value.length < 1) return {pages: true, pagesError: "Pages cannot be in negative number"}
+          if(value > 100000) return {pages: true, pagesError: "Pages must be less than 100000"}
+          return {pages: false, pagesError: false}
+      }
 }
 
 export default validate;
