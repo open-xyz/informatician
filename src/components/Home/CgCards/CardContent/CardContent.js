16a29b30c56c7844030732a1428403165520e3cd
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-07-17 22:38:11 +0530

    Proper Routing of categories

--- src/components/Home/CgCards/CardContent/CardContent.js
+++ src/components/Home/CgCards/CardContent/CardContent.js
@@ -4,7 +4,7 @@ import "./CardContent.css";
 export default function CardContent(props) {
   return (
     <>
-      <Link to="/pcategories/biography">
+      <Link to={props.link}>
         <div className="cg-card flex flex-row align-items-center mx-2 px-3 py-2 h-20 position-relative cursor-pointer border">
             <p className="w-auto">{props.title}</p>
 
