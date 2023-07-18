4b5c004dfe0ea7e83ca88621f36e0b2af78a2303
Tushar Saini <s2sharpit@gmail.com>
2023-07-18 14:17:25 +0530

    also making responsive
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,6 @@
-"use client"
-
-import React, { useState, useEffect } from "react";
+'use client'
+import { useState, useEffect } from "react";
 import { FaDiscord, FaTwitter, FaGithub } from "react-icons/fa";
-import styles from "@/styles/Contact.module.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import validate from "@/utils/validation";
@@ -25,11 +23,11 @@ export default function Contact() {
     email: true,
     message: true,
   });
-  const [success, setsuccess] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleResponse = async (e) => {
     e.preventDefault();
-    setsuccess("");
+    setSuccess("");
     let submitable = true;
     Object.values(error).forEach((err) => {
       if (err !== false) {
@@ -46,9 +44,13 @@ export default function Contact() {
         },
         body: JSON.stringify(form),
       });
-      setsuccess("Contact Request sent successfully!!");
+      setSuccess("Contact Request sent successfully!!");
     } else {
-      toast.error("Please fill the form correctly", { position: 'top-center', autoClose: 3000, theme: 'colored' })
+      toast.error("Please fill the form correctly", {
+        position: "top-center",
+        autoClose: 3000,
+        theme: "colored",
+      });
     }
   };
 
@@ -64,39 +66,33 @@ export default function Contact() {
   };
 
   return (
-    <div className={`${styles.contact}`}>
-      <div className="container">
-        <div className={styles.c_head}>
-          <h1
-            className={styles["contact-title"]}
-            data-aos="fade-down"
-            data-aos-duration="500"
-          >
-            Contact Us
-          </h1>
-          <p
-            className={styles["contact-description"]}
-            data-aos="fade-down"
-            data-aos-duration="500"
-          >
-            We&apos;d love to hear from you! Reach out to us for any inquiries,
-            feedback, or collaboration opportunities.
-          </p>
-        </div>
+    <div className="container max-w-5xl mt-16 py-16 mx-auto px-4">
+      <div
+        className="text-center"
+        data-aos="fade-down"
+        data-aos-duration="500"
+      >
+        <h1 className="text-5xl font-bold text-blue-600 mb-6">
+          Contact Us
+        </h1>
+        <p className="text-lg text-gray-500 mb-12">
+          We'd love to hear from you! Reach out to us for any inquiries,
+          feedback, or collaboration opportunities.
+        </p>
+      </div>
 
-        <div className={`${styles["contact-content"]} dark:bg-black`}>
-          <div className={styles["contact-text"]}>
+      <div className="border shadow-lg rounded-lg p-8 dark:shadow-gray-300/30">
+        <div className="flex items-center justify-between gap-8 max-md:flex-col-reverse">
+          <div className="w-full md:w-1/2">
             {success && (
-              <p className={`${styles["text-green-600"]} ${styles["text-18"]} ${styles["font-18"]} ${styles["text-left"]} mb-3`}>
-                {success}
-              </p>
+              <p className="text-green-600 text-2xl mb-3">{success}</p>
             )}
 
-            <form className={styles["contact-form"]} onSubmit={handleResponse} role="form">
-              <div className={styles["form-field"]}>
+            <form onSubmit={handleResponse} role="form">
+              <div className="mb-6">
                 <label
                   htmlFor="name"
-                  className="block uppercase tracking-wide text-lg font-bold mb-2 text-left"
+                  className="block uppercase tracking-wide text-lg font-bold mb-2"
                   data-aos="fade-left"
                   data-aos-duration="500"
                 >
@@ -107,7 +103,7 @@ export default function Contact() {
                   id="name"
                   name="name"
                   placeholder="Name"
-                  className={`${styles["form-input"]} focus:ring-blue-600 focus:border-blue-600`}
+                  className="w-full px-4 py-2 border border-gray-300 rounded focus:ring-blue-600 focus:border-blue-600"
                   required
                   value={form.name}
                   onChange={handleChange}
@@ -119,15 +115,18 @@ export default function Contact() {
                   aria-invalid={error.name && error.nameError ? "true" : "false"}
                 />
                 {error.name && error.nameError ? (
-                  <AuthErrorMessage message={error.nameError} role="alert"
+                  <AuthErrorMessage
+                    message={error.nameError}
+                    role="alert"
                     id="nameError"
-                    aria-live="assertive" />
+                    aria-live="assertive"
+                  />
                 ) : null}
               </div>
-              <div className={styles["form-field"]}>
+              <div className="mb-6">
                 <label
                   htmlFor="email"
-                  className="block uppercase tracking-wide text-lg font-bold mb-2 text-left"
+                  className="block uppercase tracking-wide text-lg font-bold mb-2"
                   data-aos="fade-left"
                   data-aos-duration="500"
                 >
@@ -138,7 +137,7 @@ export default function Contact() {
                   id="email"
                   name="email"
                   placeholder="Email"
-                  className={`${styles["form-input"]} focus:ring-blue-600 focus:border-blue-600`}
+                  className="w-full px-4 py-2 border border-gray-300 rounded focus:ring-blue-600 focus:border-blue-600"
                   required
                   value={form.email}
                   onChange={handleChange}
@@ -150,15 +149,18 @@ export default function Contact() {
                   aria-invalid={error.email && error.emailError ? "true" : "false"}
                 />
                 {error.email && error.emailError ? (
-                  <AuthErrorMessage message={error.emailError} role="alert"
+                  <AuthErrorMessage
+                    message={error.emailError}
+                    role="alert"
                     id="emailError"
-                    aria-live="assertive" />
+                    aria-live="assertive"
+                  />
                 ) : null}
               </div>
-              <div className={styles["form-field"]}>
+              <div className="mb-6">
                 <label
                   htmlFor="message"
-                  className="block uppercase tracking-wide text-lg font-bold mb-2 text-left"
+                  className="block uppercase tracking-wide text-lg font-bold mb-2"
                   data-aos="fade-left"
                   data-aos-duration="500"
                 >
@@ -168,7 +170,7 @@ export default function Contact() {
                   id="message"
                   placeholder="Message"
                   name="message"
-                  className={`${styles["form-input"]} ${styles["message-input"]} focus:ring-blue-600 focus:border-blue-600`}
+                  className="w-full px-4 py-2 border border-gray-300 rounded resize-none focus:ring-blue-600 focus:border-blue-600"
                   required
                   value={form.message}
                   onChange={handleChange}
@@ -180,16 +182,17 @@ export default function Contact() {
                   aria-invalid={error.message && error.messageError ? "true" : "false"}
                 />
                 {error.message && error.messageError ? (
-                  <AuthErrorMessage message={error.messageError} role="alert"
+                  <AuthErrorMessage
+                    message={error.messageError}
+                    role="alert"
                     id="messageError"
-                    aria-live="assertive" />
+                    aria-live="assertive"
+                  />
                 ) : null}
               </div>
               <button
-                data-aos="fade-up"
-                data-aos-duration="500"
                 type="submit"
-                className={styles["submit-btn"]}
+                className="w-full py-2 px-4 text-white font-semibold bg-blue-600 rounded hover:bg-blue-700 transition-colors duration-300"
                 onClick={handleResponse}
               >
                 Submit
@@ -197,36 +200,38 @@ export default function Contact() {
             </form>
           </div>
           <div
-            className={styles["contact-image"]}
+            className="w-full md:w-1/2 flex items-center"
             data-aos="fade-right"
             data-aos-duration="500"
           >
             <Image
-              style={{ marginLeft: "10%" }}
               width={600}
               height={600}
               src={"/assets/mainPages/contact-us.webp"}
               alt="Contact"
-              className={styles["contact-image"]}
+              className="w-full"
             />
           </div>
         </div>
-        <div className={styles["contact-social-icons"]}>
-          <h1 style={{ marginTop: 20, marginBottom: 20, fontSize: 30 }}>
-            Connect with us:
-          </h1>
-          <div className={styles["social-icons"]}>
-            <Link href="/develop" className={styles["social-icon-twitter"]}>
+        <div className="text-center mt-20">
+          <h1 className="text-2xl mb-8">Connect with us:</h1>
+          <div className="flex items-center justify-center space-x-4">
+            <Link
+              href="/develop"
+              target="_blank"
+              className="flex items-center justify-center w-10 h-10 bg-black text-white dark:bg-white dark:text-black rounded-full transition-all duration-300 hover:bg-blue-500">
               <FaTwitter />
             </Link>
             <Link
               href="https://github.com/rohansx/informatician"
               target="_blank"
-              className={styles["social-icon-github"]}
-            >
+              className="flex items-center justify-center w-10 h-10 bg-black text-white dark:bg-white dark:text-black rounded-full transition-all duration-300 hover:bg-gray-300">
               <FaGithub />
             </Link>
-            <Link href="/develop" className={styles["social-icon-discord"]}>
+            <Link
+              href="/develop"
+              target="_blank"
+              className="flex items-center justify-center w-10 h-10 bg-black text-white dark:bg-white dark:text-black rounded-full transition-all duration-300 hover:bg-blue-600">
               <FaDiscord />
             </Link>
           </div>
