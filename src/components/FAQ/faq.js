821d046c863c098ed1319ccec4a54d3d75ffaa7f
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2023-07-24 21:21:23 +0530

    FAQ Reusable Code

--- src/components/FAQ/faq.js
+++ src/components/FAQ/faq.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./faq.css";
 import helpImage from "../../assets/mainPages/help.webp";
+import faqData from "./faqData";
 
 const AccordionItem = ({ title, content}) => {
   const [expanded, setExpanded] = useState(false);
@@ -49,28 +50,13 @@ const FaqHelp = () => {
         <div className="faqcontainer">
         <h2 className="faqh2">Frequently Asked Questions</h2>
         <div className="accordion">
-          <AccordionItem
-            title="Q: How can one develop a habit of reading regularly?"
-            content="A: One can start by setting aside dedicated time for reading
-            each day. Creating a comfortable reading environment and choosing
-            books that genuinely interest you can also help."
+        {faqData.map((faq, index)=>{
+          return <AccordionItem key={index}
+            title={faq.title}
+            content={faq.content}
           />
-          <AccordionItem
-            title=" Q: What are some common reasons why people abandon books midway?"
-            content=" A: Some reasons include a lack of interest in the content or
-            finding it difficult to connect with the writing style. However,
-            there are many other books to choose from."
-          />
-           <AccordionItem
-            title=" Q:What are the benefits of reading regularly?"
-            content=" A: Reading regularly has numerous benefits : improved cognitive function, increased knowledge, and enhanced focus & concentration.
-            It reduces stress and promotes relaxation by immersing you in a different world or story.
-            Reading improves ones vocabulary, language skills, and writing abilities, making one an effective communicator."
-          />
-           <AccordionItem
-            title=" Q:What are some strategies for staying motivated to read?"
-            content=" A: To stay motivated to read, it can be helpful to set reading goals and establish a regular reading routine.Additionally, having a consistent reading routine helps to make reading a habit and ensures one to set aside dedicated time for it."
-          />
+        })}
+         
         </div>
       </div>
         </div>
