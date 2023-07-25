fbc1c6e3b8350963b990e0220b353add3ac20fd4
Jayesh Yadav <85815172+Nishitbaria@users.noreply.github.com>
2023-07-25 11:01:17 +0530

    Fixed Css and styling

--- src/pages/Login/GoogleLogin.jsx
+++ src/pages/Login/GoogleLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
-
+import GoogleLogo from "../../assets/googleLogo.png";
 import axios from 'axios';
 import WelcomeUser from './Welcomeuser';
 const  GooglePage = ({SetisLoggedIn}) => {
@@ -54,10 +54,19 @@ const  GooglePage = ({SetisLoggedIn}) => {
         <div >
           <WelcomeUser user={profile} />
           <br />
-          <br />
-          <button onClick={logOut}>Log out</button>
+        
+          <button
+  className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md transition duration-300 ease-in-out"
+  onClick={logOut}
+>
+  Log out
+</button>
         </div>
-      ):(          <button onClick={login}>LogIn With Google</button>)
+      ):(         
+      <button className="w-[100%] flex justify-center items-center gap-2 bg-red-600 text-white px-4 py-2 shadow-md rounded-md cursor-pointer"onClick={login}>
+      Login with Google{" "}
+      <img src={GoogleLogo} alt="" className="h-6 w-6" />
+    </button>)
 }
 </div>
   )
