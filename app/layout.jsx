d5c95830c1797aec6b5a44256c2a3c7d78303d8e
Nishitbaria <85815172+Nishitbaria@users.noreply.github.com>
2023-08-07 23:45:57 +0530

    Added Loading animation

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,9 @@
 /** @format */
+"use client"
 
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
-
+import Preloader from "@/components/Preloader/Preloader"; // Import the Preloader component
 const Navbar = dynamic(() => import("@/components/Navbar/Navbar"));
 import dynamic from "next/dynamic";
 import Footer from "@/components/Footer/Footer";
@@ -11,26 +12,41 @@ import ScrollToTopButton from "@/components/ScrollButton/ScrollButton";
 import { Suspense } from "react";
 import Provider from "@/context/AuthContext";
 import { Toaster } from "react-hot-toast";
+import { useState,useEffect } from "react";
 
 // Import & exporting the metadata
 import { metadata } from "./metadata";
-export {metadata}
+export { metadata }
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3200);
+  }, []);
   return (
     <html lang="en">
       <body className={`${inter.className} dark:bg-neutral-950`}>
         <Provider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Navbar />
-            </Suspense>
-            <Toaster />
-            {children}
-            <Footer />
-            <ScrollToTopButton />
+            {/* Add the Preloader component here */}
+      {loading ? (
+    <Preloader />
+  ) : (
+  <>
+   <Suspense fallback={<div>Loading...</div>}>
+  <Navbar />
+</Suspense>
+<Toaster />
+{children}
+<Footer />
+<ScrollToTopButton /></>
+ )}
+
+            
           </ThemeProvider>
         </Provider>
       </body>
